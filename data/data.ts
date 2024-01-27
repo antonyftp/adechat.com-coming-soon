9ef6af3969343caa8f2c7277b1ebdcb7fcc0d126
Antony Fantapié <fantapie.antony@gmail.com>
2024-01-28 00:28:55 +0100

    misc: change typo

--- data/data.ts
+++ data/data.ts
@@ -2,7 +2,7 @@ const data = {
     sitename: "adechat.com",
     sitetagline: "",
     sitelogo: "",
-    title: "Comming Soon",
+    title: "Coming Soon",
     description: "Antony is currently working 👨‍💻 hard to bring you this website, and he can&apos;t wait to share it with you 📅. He has a lot of ideas / features for this website 🚀 that he think you&apos;ll love 😍.",
 }
 
