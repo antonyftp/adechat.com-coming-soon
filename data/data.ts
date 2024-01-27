5b8d9cc4bed690223da976052815fa3077ef7414
Antony Fantapié <fantapie.antony@gmail.com>
2024-01-28 00:26:48 +0100

    fix: fix typo && git push

--- data/data.ts
+++ data/data.ts
@@ -3,7 +3,7 @@ const data = {
     sitetagline: "",
     sitelogo: "",
     title: "Comming Soon",
-    description: "Antony is currently working 👨‍💻 hard to bring you this website, and he can&apos;t wait to share it with you 📅. He has a lot of ideas / features for this website 🚀 that we think you&apos;ll love 😍.",
+    description: "Antony is currently working 👨‍💻 hard to bring you this website, and he can&apos;t wait to share it with you 📅. He has a lot of ideas / features for this website 🚀 that he think you&apos;ll love 😍.",
 }
 
 export default data;
